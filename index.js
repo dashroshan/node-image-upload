fa65cdbede024406e5b9db0d98d68812b25de99a
Roshan Dash <roshan1337d@gmail.com>
2022-09-03 00:00:32 +0530

    random name part from time

--- index.js
+++ index.js
@@ -4,19 +4,11 @@ const sharp = require("sharp");
 const fs = require("fs");
 
 const app = express();
-const upload = multer(multer.memoryStorage());
+app.use(express.static(__dirname + "/public"));
+
 const uploadDir = "uploads";
-
+const upload = multer(multer.memoryStorage());
 app.use(`/${uploadDir}`, express.static(uploadDir));
-app.use(express.static(__dirname + '/public'));
-
-function randomString(size) {
-    let charset = "abcdefghijklmnopqrstuvwxyz";
-    let randomStr = "";
-    for (let i = 0; i < size; i++)
-        randomStr += charset[Math.floor(Math.random() * charset.length)];
-    return randomStr;
-}
 
 app.post("/uploadImage", upload.single("uploadImage"), async (req, res) => {
     fs.access(uploadDir, (error) => {
@@ -24,7 +16,7 @@ app.post("/uploadImage", upload.single("uploadImage"), async (req, res) => {
             fs.mkdirSync(uploadDir);
     });
     const { buffer, originalname } = req.file;
-    const fileName = `${randomString(10)}-${originalname}.webp`;
+    const fileName = `${new Date().getTime()}${Math.floor(Math.random() * 1000)}-${originalname}.webp`;
     await sharp(buffer)
         .resize({ height: 500, width: 500 })
         .webp({ quality: 100 })
